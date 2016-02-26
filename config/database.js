1c24469a88450aa5efacdec9b3b472048b641626
Ernie Guaimano <ernie.guaimano@gmail.com>
2016-02-26 15:20:40 -0500

    Changed the DB URL to use EC2

--- config/database.js
+++ config/database.js
@@ -1,4 +1,5 @@
 module.exports = {
-    remoteUrl : 'mongodb://node:nodeuser@mongo.onmodulus.net:27017/uwO3mypu',
+    // remoteUrl : 'mongodb://node:nodeuser@mongo.onmodulus.net:27017/uwO3mypu',
+    remoteUrl : 'mongodb://dmmymongoad:BEVYx{oRPeW9=8LRRJtL@ec2-54-165-225-173.compute-1.amazonaws.com:27017/dummyDB',
     localUrl: 'mongodb://localhost/meanstacktutorials'
 };
